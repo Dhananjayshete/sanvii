c334df13e36791bb1eb1743aed331140e4bf2553
Dhananjay Shete <dshete071@gmail.com>
2026-03-13 23:12:44 +0530

    Added Sanvii widget files and updated widget styles

---
+++ src/app/sanvii-widget/sanvii-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sanvii-widget',
+  imports: [],
+  templateUrl: './sanvii-widget.html',
+  styleUrl: './sanvii-widget.scss',
+})
+export class SanviiWidget {
+
+}
